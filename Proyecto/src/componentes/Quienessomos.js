ec5f288ae361c41276b96d9d3b0cdfa6536d1b2f
Andrés Guerra <100784908+AndrewWar29@users.noreply.github.com>
2022-10-30 17:20:29 -0300

    Delete Proyecto/src/componentes directory

--- Proyecto/src/componentes/Quienessomos.js
+++
@@ -1,9 +0,0 @@
-const Quienessomos = () => {
-    return(
-        <div>
-            <h1> En esta sección ira el quien es Chileprops</h1>
-        </div>
-        
-        )
-}
-export default Quienessomos
