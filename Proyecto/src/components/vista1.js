6edc0b0dc39bd658c789ff823601acc4fc508be1
Andrés Guerra <100784908+AndrewWar29@users.noreply.github.com>
2022-11-10 13:25:06 -0300

    Final Navbar

--- Proyecto/src/components/vista1.js
+++ Proyecto/src/components/vista1.js
@@ -3,7 +3,7 @@ import "../estilos/Vista1.css";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 //import NavDropdown from 'react-bootstrap/NavDropdown';
-import logo from '../images/chileprops.png';
+import logo from '../images/chileprops1.png';
 import { RiContactsLine } from "react-icons/ri";
 import { AiOutlineHome } from "react-icons/ai";
 import { AiOutlinePicture } from "react-icons/ai";
